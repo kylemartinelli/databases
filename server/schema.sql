c01e7475a1d62467fc27cf513224c9630dfbb0fe
Kyle Martinelli <kylelmartinelli@gmail.com>
2022-11-16 17:36:19 -0800

    1st day

--- server/schema.sql
+++ server/schema.sql
@@ -4,7 +4,20 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  messages VARCHAR(100),
+  username VARCHAR(15),
+  roomname VARCHAR(15)
+
 );
+
+
+-- CREATE TABLE table_name (
+--     column1 datatype,
+--     column2 datatype,
+--     column3 datatype,
+--    ....
+-- );
+
 
 /* Create other tables and define schemas for them here! */
 
